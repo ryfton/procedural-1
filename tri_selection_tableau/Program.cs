46fb5a438f56369348a9305b1b53cab63e1c3e35
arfpchatelot <fchatelot@yahoo.fr>
2020-11-30 13:55:17 +0100

    Ajout du projet tri par selection d'un tableau

---
+++ tri_selection_tableau/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tri_selection_tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer le, nombre d'éléments du tableau");
+           
+            int dim = int.Parse(Console.ReadLine());
+
+            int[] tabNombre = new int[dim];
+            for (int  i = 0;  i < tabNombre.Length;  i++)
+            {
+                Console.WriteLine(" Veuillez entre le nombre numero {0}/{1} dans le tableau", (i + 1), dim);
+                tabNombre[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("votre tableau est rempli, voulez le trier?");
+            //affichage tableau non trié
+            ManipulationTab.AfficheTableau(tabNombre);
+            ManipulationTab.TriTableau(ref tabNombre);
+            //affichage tableau trié
+            ManipulationTab.AfficheTableau(tabNombre);
+       
+        }
+    }
+    class ManipulationTab
+    {
+         public static void TriTableau( ref int [] _monTab)
+        {
+            int mini;
+            int temp;
+            for (int i = 0; i <_monTab.Length-1; i++)
+            {
+                mini = _monTab[i];
+                for (int j = i+1; j < _monTab.Length; j++)
+                {
+                    if (_monTab[j]<mini)
+                    {
+                        
+                        temp =_monTab[i];
+                        _monTab[i] = _monTab[j];
+                        _monTab[j] = temp;
+
+                    }
+
+                }
+          }
+
+            Console.WriteLine("Voici le  tableau trié");
+
+
+
+        }
+        public static void AfficheTableau(int[] _tab)
+        {
+            Console.Write("[ ");
+            foreach (int element in _tab)
+            {
+                Console.Write(element + " ");
+            }
+
+
+            Console.Write(" ]");
+        }
+
+
+
+    }
+
+
+}
