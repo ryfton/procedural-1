bebc5831b2deca183b91b56b620fbbd6b484575b
tarik <73602347+ryfton@users.noreply.github.com>
2020-12-21 09:34:13 +0100

    der

---
+++ poo/poo/compte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace poo
+{
+    class compte
+    {
+        private int decouvertAutorise;
+        private string nom;
+        private int numero;
+        private int solde;
+
+        public void Compte()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Compte(int _numero, string _nom, int _solde, int _decouvertAutoriser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Crediter(int _montant)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Debiter(int _montant)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Superieur(int Compte_autreCompte)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Transferer(int _montant, string Compte_compteDestinataire)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
